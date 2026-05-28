49adc0a00d29ac345c54453781e33864204eb3e5
Christian Kopač <christian.kopac@dewesoft.com>
2026-05-28 19:19:24 +0200

    chore: drop leftover Vite/Tailwind/Cloudflare Pages config
    These were already implicitly removed in 5edae1b but the deletions
    weren't staged. Cleaning up now so the tree matches the Remix 3 layout.

--- functions/[[path]].ts
+++
@@ -1,6 +0,0 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-
-// @ts-expect-error - the server build is generated at build time by Remix.
-import * as build from "../build/server";
-
-export const onRequest = createPagesFunctionHandler({ build });
